f40c375edff7bafe6d36fc4e8164c0147d6ad4da
Izma Khan <71832894+Izma810@users.noreply.github.com>
2022-01-27 23:03:56 +0530

    Initial commit

---
+++ tk1.py
@@ -0,0 +1,9 @@
+from tkinter import*
+
+root=Tk()
+frame=Frame(root)
+frame.pack()
+button=Button(frame,text='Greek')
+button.pack()
+
+root.mainloop()
---
+++ tk4.py
@@ -0,0 +1,12 @@
+from tkinter import*
+
+top=Tk()
+top.geometry('450x300')
+user_name=Label(top,text="Username").place(x=40,y=100)
+submit_button=Button(top,text="Submit").place(x=40,y=130)
+user_name_input_area=Entry(top,width=30).place(x=110,y=60)
+user_passwaord_entry_area=Entry(top,width=30).place(x=110,y=100)
+
+top.mainloop()
+
+
